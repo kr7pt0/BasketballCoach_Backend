d6daa926b862a8a2de03cbf2993943dd5e4389a3
voyager-gold <eugenemathew93@gmail.com>
2019-05-22 14:41:03 -0700

    comment python endpoint

--- routes/api.php
+++ routes/api.php
@@ -35,12 +35,68 @@ Route::middleware('auth:api')->group(function () {
 
         // Rest for python
 
+        /*
+         *  Check if other user is in game session
+         *  This is the endpoint for frontend, no need to be called by python
+         *  as on-coming game start endpoint will check legitimacy for game creation
+         * 
+         *  { 'status': 'free' }
+         *  { 'status': 'occupied, 'user': 'Jack' }
+         * 
+         */
         Route::get('/game/check', 'GameController@gameCheck');
 
+        /*
+         *  Start game if nobody plays game
+         * 
+         *  @params
+         *      mode: FREE_THROW | DRILLS
+         * 
+         *  { 'status': 'ok' }
+         *  { 'status': 'invalid parameters' }, 400
+         *  { 'status': 'occupied, 'user': 'Jack' }, 403
+         * 
+         */
         Route::post('/game/start', 'GameController@gameStart');
 
+        /*
+         *  Cancel game if use has started game
+         * 
+         *  { 'status': 'ok' }
+         *  { 'status': 'not started' }, 403
+         * 
+         */
         Route::post('/game/cancel', 'GameController@gameCancel');
 
+        /*
+         *  Save game into db if python submits game data user has just played
+         *  JSON Request should be made for this endpoint
+         * 
+         *  @header
+         *      Content-Type:   application/json
+         * 
+         *  @params
+         *    {
+         *      releaseAngle
+         *      releaseTime
+         *      elbowAngle
+         *      legAngle
+         *      tryCount
+         *      score
+         *      positions [
+         *          {
+         *              x
+         *              y
+         *              success: 0 | 1
+         *          }
+         *      ]
+         *    }
+         * 
+         *  { 'status': 'ok' }
+         *  { 'status': 'invalid parameters' }, 400
+         *  { 'status': 'not started' }, 403
+         * 
+         */
         Route::post('/game', 'GameController@saveGame');
     
     });
